9510fc5af0dc42fd239ebb65623e251a2bd6fa8a
Vidya Singh <bhushanjss@gmail.com>
2018-08-08 03:09:39 -0400

    login action refractor

--- src/actions/forms/index.js
+++ src/actions/forms/index.js
@@ -1,35 +1,23 @@
 import firebase from 'firebase';
 import { Actions } from 'react-native-router-flux';
 
+import action from '../action';
 import { EMAIL_CHANGE, PASSWORD_CHANGE, CONFIRM_PASSWORD_CHANGE,
-  LOGIN_USER_SUCCESS, LOGIN_USER_FAILED,
-  LOGIN_USER, CREATE_USER, TOGGLE_ACCOUNT } from './types';
+  LOGIN_USER_SUCCESS, LOGIN_USER_FAILED, LOGIN_USER, CREATE_USER, CREATE_USER_SUCCESS,
+  TOGGLE_ACCOUNT, FIRST_NAME_CHANGE, LAST_NAME_CHANGE, PHONE_NUMBER_CHANGE, PROFILE_STREET_CHANGE,
+  PROFILE_APT_CHANGE, PROFILE_CITY_CHANGE, PROFILE_STATE_CHANGE, PROFILE_ZIP_CHANGE,
+  CAR_MODEL_CHANGE, CAR_SEATS_CHANGE, SAVE_PROFILE, SAVE_PROFILE_SUCCESS, SAVE_PROFILE_FAILED } from './types';
 
-//Login Login Form
+//Login Form
 
-export const emailChange = (text) => ({
-  type: EMAIL_CHANGE,
-  payload: text
-});
-
-export const passwordChange = (text) => ({
-  type: PASSWORD_CHANGE,
-  payload: text
-});
-
-export const confirmPasswordChange = (text) => ({
-  type: CONFIRM_PASSWORD_CHANGE,
-  payload: text
-});
-
-export const toggleAccount = (val) => ({
-  type: TOGGLE_ACCOUNT,
-  payload: val
-});
+export const emailChange = (text) => action(EMAIL_CHANGE, text);
+export const passwordChange = (text) => action(PASSWORD_CHANGE, text);
+export const confirmPasswordChange = (text) => action(CONFIRM_PASSWORD_CHANGE, text);
+export const toggleAccount = (val) => action(TOGGLE_ACCOUNT, val);
 
 export const loginUser = ({ email, password }) => (
    (dispatch) => {
-    dispatch({ type: LOGIN_USER });
+    dispatch(action(LOGIN_USER));
     return firebase.auth().signInWithEmailAndPassword(email, password)
     .then(user => loginUserSuccess(dispatch, user))
     .catch(error => loginUserFailed(dispatch, error));
@@ -38,18 +26,30 @@ export const loginUser = ({ email, password }) => (
 
 export const createUser = ({ email, password }) => (
   (dispatch) => {
-   dispatch({ type: CREATE_USER });
+   dispatch(action(CREATE_USER));
    firebase.auth().createUserWithEmailAndPassword(email, password)
-   .then(user => loginUserSuccess(dispatch, user))
+   .then(user => createUserSuccess(dispatch, user))
    .catch(error => loginUserFailed(dispatch, error));
  }
 );
 
 const loginUserSuccess = (dispatch, user) => {
-  dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
-  Actions.showFlights({ type: 'reset' });
+  dispatch(action(LOGIN_USER_SUCCESS, user));
+  Actions.profile({ type: 'reset' });
+};
+
+const createUserSuccess = (dispatch, user) => {
+  dispatch(action(CREATE_USER_SUCCESS, user));
+  Actions.profileForm({ type: 'reset' });
 };
 
 const loginUserFailed = (dispatch, error) => (
-  dispatch({ type: LOGIN_USER_FAILED, payload: error })
+  dispatch(action(LOGIN_USER_FAILED, error))
 );
+
+//Profile forms
+
+export const firstNameChange = (text) => ({
+  type: FIRST_NAME_CHANGE,
+  payload: text
+});
